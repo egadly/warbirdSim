2e7f90cb8f67d6d02db48e853c9ec483c5db8b91
egadly <ernest.gaddi.491@my.csun.edu>
2017-12-10 19:07:16 -0800

    just in case

--- warbirdSim/simpleFragment.glsl
+++ warbirdSim/simpleFragment.glsl
@@ -8,7 +8,7 @@ in vec3 vs_normal;
 in vec3 vs_lightpos;
         
 // define light propoerties
-uniform vec4 color_ambient = vec4(0.1, 0.1, 0.50, 1.0); //Ambient Color
+uniform vec4 color_ambient = vec4(0.1, 0.1, 1.0, 1.0); //Ambient Color
 
 uniform sampler2D Texture; //Texture input
 uniform bool IfRuber; //Ruber Check for brightening
@@ -20,7 +20,7 @@ void main(void) {
    float ambient = 0.15f;   // Ambient Light Weight
    vec3 vs_light_direction = normalize(vs_lightpos - vs_worldpos); // Determine Light Direction
    vec3 normal = normalize(vs_normal);
-   float diffuse_cam = 0.334f*max(0.0, dot(normal, light_direction)); //Camera Directional Light
+   float diffuse_cam = 0.25f*max(0.0, dot(normal, light_direction)); //Camera Directional Light
    float diffuse_point = min( 5.0f, max( 0.0, 1000.0f*dot(normal,vs_light_direction)) ); //Ruber Point Light
    float diffuse_ruber = 0.0f; //If not Ruber use this
    if ( IfRuber ) diffuse_ruber = 0.15f; //If Ruber use this
@@ -28,6 +28,6 @@ void main(void) {
     color = texture(Texture,vec2( gl_FragCoord.x/800.0f, gl_FragCoord.y/600.0f) ); //Texture coordinate is based on Camera Position
    }
    else {
-     color = ambient * (color_ambient + vsColor)/2.0f + (1.0f - ambient) * (diffuse_cam + diffuse_point + diffuse_ruber) * vsColor; //Ambient weight + diffuse weight
+     color = ambient * (color_ambient) + (1.0f - ambient) * (diffuse_cam + diffuse_point + diffuse_ruber) * vsColor; //Ambient weight + diffuse weight
    }
 }
